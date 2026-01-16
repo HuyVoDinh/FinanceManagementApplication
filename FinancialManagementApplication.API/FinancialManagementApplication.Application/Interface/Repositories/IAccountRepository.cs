224c388dcac48d8ec116d4c8122e80683f59cc9a
Wistfy <huy.vd76@gmail.com>
2026-01-16 21:58:51 +0700

    Update file path, file name

---
+++ FinancialManagementApplication.API/FinancialManagementApplication.Application/Interface/Repositories/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using FinanceManagementApplication.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace FinanceManagementApplication.Application.Interface.Repositories
+{
+    public interface IAccountRepository
+    {
+        Task<Account?> GetByEmailAsync(string email);
+        Task AddAsync(Account account);
+    }
+}
---
+++ FinancialManagementApplication.API/FinancialManagementApplication.Domain/Entities/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagementApplication.Domain.Entities
+{
+    public class Account
+    {
+        public Guid AccountID { get; set; }
+        public string email { get; set; }
+        public string passwordHash { get; set; }    
+        public DateTime CreateAt { get; set; }
+    }
+}
